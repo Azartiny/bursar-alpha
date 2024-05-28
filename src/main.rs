516ab614495d43803d098d09a98ea894d6574029
Azartiny <80139282+Azartiny@users.noreply.github.com>
2024-05-29 08:36:05 +1200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::f64;
 use gio::prelude::*;
 use glib::clone;
 use gtk4::prelude::*;
@@ -5,13 +6,14 @@ use gtk4::{
     Application, ApplicationWindow, Box, Button, Entry, FileChooserAction, FileChooserDialog,
     Label, Orientation, ResponseType, Window,
 };
-use core::f64;
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::rc::Rc;
 
 mod save_data;
 
 fn main() {
-
-    
     //Инициализация
     let app = Application::new(
         Some("com.kaznachey.gtk4"),
@@ -23,42 +25,40 @@ fn main() {
     app.run();
 }
 
-
 fn master_ui(app: &Application) {
     //Основное окно
     let window = ApplicationWindow::new(app);
     window.set_title(Some("Казначей альфа (демоверсия)"));
-    window.set_default_size(700, 800);
+    window.set_default_size(400, 200);
 
-    
-    
-    let vbox = Box::new(Orientation::Vertical, 12);      //Вертикальный контейнер
-    
+    let vbox = Box::new(Orientation::Vertical, 12); //Вертикальный контейнер
+
     //Метка и поле ввода ФИО пользователя
     let hbox_klient = Box::new(Orientation::Horizontal, 5);
     let label_klient = Label::new(Some("Введите имя:"));
     let intro_klient = Entry::new();
 
+    //ИНН ИП
+    let hbox_inn = Box::new(Orientation::Horizontal, 5);
+    let label_inn = Label::new(Some("ИНН:"));
+    let entry_inn = Entry::new();
 
     //Метки и поля ввода для A (Доход) и B (Фикса)
     let hbox_label_a: Box = Box::new(Orientation::Horizontal, 5);
     let label_a = Label::new(Some("Введите Доход:"));
     let entry_a = Entry::new();
 
-
     let hbox_label_b: Box = Box::new(Orientation::Horizontal, 5);
     let label_b = Label::new(Some("Введите Фикс:"));
     let entry_b = Entry::new();
 
-
-   
-
-    //Метку Рассчитать
-    let label_result = Label::new(Some("Имя: \rБаза: \rНалог: \rСумма к выплате:"));
+    //Меткa Рассчитать
+    let label_result = Label::new(Some("Имя: \rИНН: \rБаза: \rНалог: \rСумма к выплате:"));
 
     //Кнопка Рассчитать
     let calculate_button = Button::with_label("Рассчитать");
-    calculate_button.connect_clicked(clone!(@strong intro_klient, @strong entry_a, @strong entry_b, @strong label_result => move |_| {
+    calculate_button.connect_clicked(clone!(@strong intro_klient, @strong entry_inn, @strong entry_a, @strong entry_b, @strong label_result => move |_| {
+        let result_inn = entry_inn.text().to_string();
         let result_klient = intro_klient.text().to_string();
         let a: f64 = entry_a.text().parse().unwrap_or(0.0);
         let b: f64 = entry_b.text().parse().unwrap_or(0.0);
@@ -67,34 +67,42 @@ fn master_ui(app: &Application) {
         let ceiled_nalog_d = nalog_d.ceil(); // Округление в большую сторону до целого числа
         let payment: f64 = nalog_d + b; //Сумма к выплате (Налог + Фикса)
         let ceiled_payment = payment.ceil();//Округление ceil
-        label_result.set_text(&format!("Имя: {}\rБаза: {} ₽\rНалог: {} ₽ \rСумма к выплате: {} ₽", result_klient, baza_c, ceiled_nalog_d, ceiled_payment));
+        label_result.set_text(&format!("Имя: {} \rИНН: {} \rБаза: {} ₽\rНалог: {} ₽ \rСумма к выплате: {} ₽", result_klient, result_inn, baza_c, ceiled_nalog_d, ceiled_payment));
     }));
 
-    // Кнопка очищения от всякого
+    //Кнопка очищения от всякого
     let clear_button = Button::with_label("Очистить");
-    clear_button.connect_clicked(clone!(@strong entry_a, @strong entry_b, @strong intro_klient, @strong label_result => move |_| {
+    clear_button.connect_clicked(clone!(@strong entry_inn, @strong entry_a, @strong entry_b, @strong intro_klient, @strong label_result => move |_| {
+
+        entry_inn.set_text("");
         entry_a.set_text("");
         entry_b.set_text("");
         intro_klient.set_text("");
         label_result.set_text("");
     }));
 
-    //Кнопка сохранения данных в текстовик
-    let save_button = Button::with_label("Сохранить в текстовый файл");
-    save_button.connect_clicked(clone!(@strong intro_klient, @strong label_result => move |_| {
-        let text = label_result.text().as_str().to_string();
-        save_data::save_to_file(&text, "Отчёт.txt").expect("Не удалось сохранить данные");
-    }));
+    //Кнопка сохранения данных
+    let save_button = Button::with_label("Сохранить файл");
+    save_button.connect_clicked(
+        clone!(@strong intro_klient, @strong label_result => move |_| {
+            let text = label_result.text().as_str().to_string();
+            save_data::save_to_file(&text, "Отчёт.txt").expect("Не удалось сохранить данные");
+        }),
+    );
 
     // Добавляем все виджеты в контейнер
+
     hbox_klient.append(&label_klient);
     hbox_klient.append(&intro_klient);
+    hbox_inn.append(&label_inn);
+    hbox_inn.append(&entry_inn);
     hbox_label_a.append(&label_a);
     hbox_label_a.append(&entry_a);
     hbox_label_b.append(&label_b);
     hbox_label_b.append(&entry_b);
 
     vbox.append(&hbox_klient);
+    vbox.append(&hbox_inn);
     vbox.append(&hbox_label_a);
     vbox.append(&hbox_label_b);
     vbox.append(&label_result);
@@ -103,7 +111,5 @@ fn master_ui(app: &Application) {
     vbox.append(&clear_button);
     window.set_child(Some(&vbox));
 
-    // window.present();
     window.show();
 }
-
